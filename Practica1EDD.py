5f1603676418e0bc5f7ac36cd90c5cebc559ebc6
nickolasnolte1 <61555652+nickolasnolte1@users.noreply.github.com>
2021-01-15 16:35:34 -0600

    Código con profiling

---
+++ Practica1EDD.py
@@ -0,0 +1,16 @@
+import cProfile
+
+def sumatoria(numero):
+    if(numero == 0):
+        return numero
+    else:
+        return (numero * (numero + 1) / 2)
+    
+number = int(input("Escriba un número entero positivo: "))
+
+valortotal = sumatoria(number)
+
+print("La sumatoria de los números naturales del 1 al {0} es: {1}".format(number, valortotal))
+
+
+cProfile.run ('sumatoria(5)')
